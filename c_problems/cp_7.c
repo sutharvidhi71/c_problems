79476af94077e2fdd658eed6dc373681b133dbdb
sutharvidhi71 <sutharvidhi71@gmail.com>
2025-12-11 23:00:59 +0530

    pettern 1

--- c_problems/cp_7.c
+++ c_problems/cp_7.c
@@ -1,19 +1,26 @@
+
+//reverse the given number 
+
 #include<stdio.h>
 int main(){
      int n;
      long long reverse=0;
 
      printf("enter number:\n");
-     if(scanf("%d",&n)!=1){
+
+     if(scanf("%d",&n)!=1)//cheks if input ids not invalid
+     {
         printf("invelid input:\n");
         return 1;
      }
-     int sigh=(n<0)?-1:1;
+
+     int sigh=(n<0)?-1:1;//to store sign of number
      long long temp=(n<0)?-(long long )n:(long long )n;
+
      while(temp>0){
-        int digit=temp%10;
-        reverse=reverse*10+digit;
-        temp/=10;
+        int digit=temp%10;//to extract last digit
+        reverse=reverse*10+digit;// by multiplying reverse to 10 it push the digit ot left and make space for coming digit
+        temp/=10;//removes last degit
 
      }
      reverse*=sigh;
@@ -21,3 +28,6 @@ int main(){
 
     return 0;
 }
+
+//this logic looked really hard first but after trying
+ //again and again makes it easy to understand
