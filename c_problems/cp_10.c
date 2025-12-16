94f5da76c09d1e1d638711017fa793db86c1f195
sutharvidhi71 <sutharvidhi71@gmail.com>
2025-12-16 21:57:21 +0530

    pettern 3

---
+++ c_problems/cp_10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int n;
+int mun=1;
+printf("enter number of rows:-");
+scanf("%d",&n);
+
+for(int i=1;i<=n;i++)//controls the row of pettern
+{
+    for(int j=1;j<=i;j++)//printf the pettern in rows
+    {
+        printf("%d",mun++);
+    }
+    printf("\n");//to start new row
+}
+    return 0;
+}
--- c_problems/cp_9.c
+++ c_problems/cp_9.c
@@ -1,3 +1,6 @@
+//another pettern just the change is that it prints number instead of stars as you
+//know we can always print any sings we just have to put it in in the print function.
+
 #include <stdio.h>
 
 int main(){
@@ -5,11 +8,14 @@ int row;
 printf("enter the number of row:-");
 scanf("%d",&row);
 
-for(int i=1;i<=row;i++){
-    for(int j=1;j<=i;j++){
+for(int i=1;i<=row;i++)//controls the row of pettern
+{
+    for(int j=1;j<=i;j++)//printf the pettern in rows
+    {
         printf("%d",j);
     }
-    printf("\n");
+    printf("\n");//to start new row
 }
     return 0;
 }
+
