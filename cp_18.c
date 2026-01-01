94d4508ff8ccde2c2ed57c089b3c693aa0dd115e
sutharvidhi71 <sutharvidhi71@gmail.com>
2026-01-01 18:56:04 +0530

    reverse string

--- cp_18.c
+++ cp_18.c
@@ -1,1 +1,24 @@
-#
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("Enter element %d:",i+1);
+        scanf("%d",&arr[i]);
+    }
+    int max=arr[0];
+    int min=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    printf("Maximum element is %d\n",max);
+    printf("Minimum element is %d\n",min);  
+    return 0;
+}
