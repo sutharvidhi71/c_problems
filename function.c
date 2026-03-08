ae5e24b71ee51838612624ad6316425185761cf8
sutharvidhi71 <sutharvidhi71@gmail.com>
2026-03-08 14:35:08 +0530

    new practice

--- function.c
+++ function.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 void swap(int *a, int *b)
 {
     int temp = *a;
@@ -9,3 +10,4 @@ int main()
     int x=5,y = 10;
     swap(&x, &y);
     printf("%d %d", x, y); // Output: 10 5 }
+}
